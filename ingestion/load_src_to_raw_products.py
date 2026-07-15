a77b0524a3ea82d7ec998ede36f30e69b7f54f3e
chauhan018 <chauhan.vk018@gmail.com>
2026-07-15 23:59:43 +0530

    commit raw layer

---
+++ ingestion/load_src_to_raw_products.py
@@ -0,0 +1,60 @@
+import os,json,requests,psycopg2
+from datetime import datetime, timezone
+from dotenv import load_dotenv
+import pandas as pd
+import uuid
+import io
+
+load_dotenv()
+
+csv_path = "ingestion/data/products_src.csv"
+table_name = "flipkart_raw.products_raw"
+run_id = str(uuid.uuid4())
+batch_id = str(uuid.uuid4())
+created_by = "python_ingestion_script"
+
+def build_dataframe_from_csv(csv_path):
+    dataset = pd.read_csv(csv_path)
+    dataset['run_id'] = run_id
+    dataset['batch_id'] = batch_id
+    dataset['dw_created_by'] = created_by
+    dataset['dw_modified_by'] = created_by
+    dataset['dw_created_date'] = datetime.now(timezone.utc)
+    dataset['dw_modified_date'] = datetime.now(timezone.utc)
+    return dataset
+
+def load_to_postgres(dataset: pd.DataFrame):
+    conn = psycopg2.connect(
+    os.environ["NEON_CONN_STRING"]
+    )
+    cur = conn.cursor()
+
+    try:
+        cur.execute(f"TRUNCATE TABLE {table_name};")
+
+        buffer = io.StringIO()
+        dataset.to_csv(buffer, index=False, header=True)
+        buffer.seek(0)
+
+        columns_sql = ", ".join(dataset.columns)
+
+        copy_sql = f"""
+                COPY {table_name} ({columns_sql})
+                FROM STDIN
+                WITH (FORMAT csv, HEADER true)
+            """
+        cur.copy_expert(copy_sql, buffer)
+
+        conn.commit()
+        print(f"Loaded {len(dataset)} rows into {table_name}. batch_id={batch_id}")
+
+    except Exception as e:
+        conn.rollback()
+        print(f"Load failed, rolled back. Error: {e}")
+        raise
+
+    finally:
+        conn.close()
+if __name__ == "__main__":
+    dataset = build_dataframe_from_csv(csv_path)
+    load_to_postgres(dataset)
